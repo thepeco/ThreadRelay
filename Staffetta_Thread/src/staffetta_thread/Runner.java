019b07d3054e482d0215bddd0794a9ae9be05fa8
thepeco <pecorelli.davide@franchettisalviani.net>
2026-04-30 16:59:44 +0200

    Creazione dell' interfaccia grafica
    Creazione metodo "farPartireGara" collegato al Runner e Gestore

--- Staffetta_Thread/src/staffetta_thread/Runner.java
+++ Staffetta_Thread/src/staffetta_thread/Runner.java
@@ -4,42 +4,61 @@
  */
 package staffetta_thread;
 
-import java.util.Random;
+import javax.swing.JProgressBar;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 public class Runner extends Thread {
 
     private int idCorridore;
-    private Gestore GestoreBox;
-    private Random rnd = new Random();
-    private int prossimo;
+    private Gestore gestoreBox;
+    private JProgressBar progressBar;
+    private JLabel labelTempo;
 
-    public Runner(int idCorridore, Gestore GestoreBox) {
+    public Runner(int idCorridore, Gestore gestoreBox, JProgressBar progressBar, JLabel labelTempo) {
         this.idCorridore = idCorridore;
-        this.GestoreBox = GestoreBox;
+        this.gestoreBox = gestoreBox;
+        this.progressBar = progressBar;
+        this.labelTempo = labelTempo;
     }
 
     @Override
     public void run() {
         try {
             while (true) {
-                GestoreBox.attendiTurno(idCorridore);
+                
+                gestoreBox.attendiTurno(idCorridore);
 
-                int tempoCorsa = 5000;
-                Thread.sleep(tempoCorsa);
+                
+                for (int i = 0; i <= 100; i++) {
+                    int progresso = i;
 
-                System.out.println("Corridore " + idCorridore + " ha terminato la sua frazione.");
+                    
+                    progressBar.setValue(progresso);
+                    labelTempo.setText(progresso + "%");
 
-                if (idCorridore == 1) {
-                    prossimo = 2;
-                } else {
-                    prossimo = 1;
+                    
+                    if (progresso == 90) {
+                        int prossimo;
+                        if (idCorridore == 1) {
+                            prossimo = 2;
+                        } else if (idCorridore == 2) {
+                            prossimo = 3;
+                        } else {
+                            prossimo = 1;
+                        }
+
+                        gestoreBox.cambiaRunner(prossimo);
+                    }
+
+                    
+                    Thread.sleep(50);
                 }
 
-                GestoreBox.cambiaRunner(prossimo);
+                labelTempo.setText("Fine");
             }
         } catch (InterruptedException e) {
-            System.out.println("Il corridore " + idCorridore + " è stato interrotto!");
-            
+            System.out.println("Thread interrotto");
         }
     }
 }
--- Staffetta_Thread/src/staffetta_thread/Staffetta_Thread.java
+++ Staffetta_Thread/src/staffetta_thread/Staffetta_Thread.java
@@ -14,7 +14,15 @@ public class Staffetta_Thread {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+
+                Gara finestra = new Gara();
+
+                finestra.setVisible(true);
+            }
+        });
     }
-    
+
 }
